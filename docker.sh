ee0076f34967f9178a35085359995b20fd6931b7
vishwaranjanpandey <vishwaranjan.p12@gmail.com>
2024-09-23 20:39:58 +0530

    docker file added and commit done

---
+++ docker.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+#---------------------------------------------#
+# Author: vishwaranjan pandey
+# Call/Whatsapp: +91-9717809862
+#---------------------------------------------#
+
+DOCKER="/usr/bin/docker"
+DRYRUN=0
+
+while [ "$#" -gt 0 ]; do
+    flag=$1
+    case $flag in 
+        --dry-run | -dry-run ) DRYRUN=1 ;;
+        -h | --h )
+                    echo "$0 --dry-run    # to run in dry mode"
+                    echo "$0              # to run in production mode"
+                    exit 0 ;;
+        * ) ;;
+esac
+shift
+done
+
+echo  "\n\n ====== Starting the Docker Clean Up Script ====== \n\n"
+
+echo  "Step 1. Checking Docker images with imageID as 'None' "
+
+nonImages=$($DOCKER images | grep -w "<none>" | awk '{print $3}')
+
+if [ "${nonImages}" != "" ]; then
+    
+    for nImage in ${nonImages}
+    do
+    echo " $nonImages "
+        if [ $DRYRUN -eq 0 ]; then
+            ${DOCKER} rmi -f ${nImage} 
+
+            if [ $? -eq 0 ]; then
+                echo "\n - Docker image with ImageId: ${nImage} Deleted Successfully \n"
+            else
+                echo "\n !! Error while deleting Docker image with ImageId: ${nImage} !!\n"  
+            fi
+        fi
+    done
+else
+    echo "\n == [ Image ID with <none> ]: No Docker Image To Delete == \n"
+fi
+
+echo  "Step 2. Removing all stopped containers"
+
+containerID=$($DOCKER ps -a | grep -w "Dead\|Exited" | awk '{print $1}')
+
+if [ "${containerID}" != "" ]; then
+    for Ocontainer in ${containerID}
+    do
+    echo "$containerID"
+        if [ $DRYRUN -eq 0 ]; then
+            $DOCKER rm ${Ocontainer}
+            if [ $? -eq 0 ]; then
+                echo "\n - [Dead|Exited] Docker container with ContainerID: ${Ocontainer} Deleted Successfully  \n"
+            else
+                   echo "\n !! [Dead|Exited] Error while deleting Docker image with COntainedID: ${Ocontainer} !! \n"  
+            fi
+        fi
+    done
+else
+    echo  "\n == There no Docker containers with status as "Exited" == \n" 
+fi  
+
+
+echo  "Step 3. Deleting old images which are two months old "
+oldImages=$($DOCKER images | awk '{print $3,$4,$5}' | grep '[5-9]\{1\}\ weeks\|years\|months' | awk '{print $1}')
+
+if [ "${oldImages}" != "" ]; then
+    for oImage in $oldImages 
+    do
+     echo "$oldImages"
+        if [ $DRYRUN -eq 0 ]; then
+             $DOCKER rmi -f ${oImage}
+            if [ $? -eq 0 ]; then
+                echo "\n -  Docker image with ImageId: ${oImage} Delted Successfully \n" 
+             else 
+                echo "\n !! Error while deleting Docker image with ImageId: ${oImage} !! \n"
+            fi
+        fi
+     done 
+else
+        echo  "\n == No Docker dangaling Images to delete == \n"
+fi                       
+
+   
+
+
+
+                
+
+
+          
+      
+
